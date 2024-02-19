41d7ed22ad9c3ab7432b29433ca4ae8c734cacb2
Lokesh-anji <62313968+Lokesh-anji@users.noreply.github.com>
2024-02-19 10:58:58 +0530

    Update automation2.sh

--- automation2.sh
+++ automation2.sh
@@ -4,3 +4,9 @@ for line in $(cat jiraid.txt)
 do
 curl -X PUT -u "your_atlassian_jira_username:your_atlassian_jira_token" --data '{"update":{"labels":[{"add":"DEMO_NEW"}]}}' -H "Content-Type: application/json" https://singam.atlassian.net/rest/api/3/issue/$line
 done 
+
+
+
+curl -X PUT -u "lokeshanji1994@gmail.com:Token" --data '{"update":{"labels":[{"add":"DEMO_NEW1"}]}}' -H "Content-Type: application/json" https://lokesh-anji.atlassian.net/rest/api/2/issue/LA-1
+
+curl -X PUT -u "lokeshanji1994@gmail.com:Token" --data '{"update":{"labels":[{"add":"DEMO_NEW"}]}}' -H "Content-Type: application/json" https://lokesh-anji.atlassian.net/rest/api/2/issue/LA-2
